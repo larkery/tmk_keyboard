b11f22ab9d52e9743a08aaf39c8363332d19c15e
Tom Hinton <t@larkery.com>
2016-06-28 07:19:10 +0100

    move arrows

--- keyboard/atreus/keymap_hinton.c
+++ keyboard/atreus/keymap_hinton.c
@@ -40,6 +40,8 @@ const uint16_t PROGMEM fn_actions[] = {
 #define KA_PIPE SHIFT(KC_NUBS)
 #define KA_DOLR SHIFT(KC_4)
 #define KA_PCNT SHIFT(KC_5)
+#define KA_PND SHIFT(KC_3)
+#define KA_PLUS SHIFT(KC_EQUAL)
 
 #define QWERTY\
   MAP( KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P, \
@@ -48,16 +50,16 @@ const uint16_t PROGMEM fn_actions[] = {
        KA_ESC, KC_TAB, KC_LGUI, KA_SHIFT, KC_BSPC, KC_SPC, KA_NUMS, KA_FUNS, KC_QUOT, KC_ENT)
 
 #define NUMS\
-  MAP( KC_7,   KC_8, KC_9,    KC_LEFT, KC_RIGHT, KA_BANG, KC_LBRC, KA_AMP,   KC_RBRC, KA_PIPE, \
-       KC_4,   KC_5, KC_6,    KC_UP,   KC_DOWN,  KC_NUHS, KA_LPAR, KA_DQUO,  KA_RPAR, SHIFT(KC_SCLN), \
-       KC_1,   KC_2, KC_3,    KC_PGUP, KC_PGDN,  KC_NUBS, KA_LBRA, KA_STAR,  KA_RBRA, SHIFT(KC_NUHS), \
-       KC_DOT, KC_0, KC_TRNS, KC_TRNS, KC_TRNS,  KA_PCNT, KA_MONUMS, KC_GRAVE, KA_DOLR, KA_HAT)
+  MAP( KC_7,   KC_8, KC_9,    KA_HAT,   KA_PCNT, KA_BANG, KC_LBRC,   KA_AMP,   KC_RBRC, KA_PIPE, \
+       KC_4,   KC_5, KC_6,    KA_PND,   KC_MINS, KC_NUHS, KA_LPAR,   KA_DQUO,  KA_RPAR, SHIFT(KC_SCLN), \
+       KC_1,   KC_2, KC_3,    KC_EQUAL, KA_PLUS, KC_NUBS, KA_LBRA,   KA_STAR,  KA_RBRA, SHIFT(KC_NUHS), \
+       KC_DOT, KC_0, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KA_MONUMS, KC_GRAVE, KA_DOLR, KC_TRNS)
 
 #define FNS                                                             \
-  MAP( KC_HELP,     KC_NO,     KC_NO, KC_NO, KC_NO,     KC_F1,  KC_F2,  KC_F3,   KC_F4,  KC_F5, \
-       KA_LOCKNUMS, KC_SYSREQ, KC_NO, KC_NO, KC_NO,     KC_F6,  KC_F7,  KC_F8,   KC_F9,  KC_F10, \
-       KC_NO,       KC_APP,    KC_NO, KC_NO, KC_INSERT, KC_F11, KC_F12, KC_F13,  KC_F14, KC_F15, \
-       KA_BOOT,     KC_NO,     KC_NO, KC_NO, KC_DELETE, KC_F16, KC_F17, KC_TRNS, KC_F18, KC_F20)
+  MAP( KC_HELP,     KC_HOME, KC_UP,   KC_END,   KC_PGUP,   KC_F1,  KC_F2,  KC_F3,   KC_F4,  KC_F5, \
+       KA_LOCKNUMS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,   KC_F6,  KC_F7,  KC_F8,   KC_F9,  KC_F10, \
+       KC_NO,       KC_APP,  KC_NO,   KC_NO,    KC_INSERT, KC_F11, KC_F12, KC_F13,  KC_F14, KC_F15, \
+       KA_BOOT,     KC_NO,   KC_NO,   KC_NO,    KC_DELETE, KC_F16, KC_F17, KC_TRNS, KC_F18, KC_F20)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QWERTY,
