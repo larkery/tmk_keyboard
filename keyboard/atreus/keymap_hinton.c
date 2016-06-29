06d476bd6d9813e2ac6b3c86af5b3f3cd595215d
Tom Hinton <tom.hinton@cse.org.uk>
2016-06-29 11:08:23 +0100

    toggle layers with layer+enter

--- keyboard/atreus/keymap_hinton.c
+++ keyboard/atreus/keymap_hinton.c
@@ -1,4 +1,5 @@
 #include "keymap_common.h"
+#include "action_layer.h"
 
 #define MAP(K00, K01, K02, K03, K04, K05, K06, K07, K08, K09,\
             K10, K11, K12, K13, K14, K15, K16, K17, K18, K19,\
@@ -14,8 +15,10 @@ const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_KEY(1, KC_MINS),
   [2] = ACTION_MODS_ONESHOT(MOD_LSFT),
   [3] = ACTION_LAYER_TAP_KEY(2, KC_EQUAL),
-  [4] = ACTION_LAYER_SET(1, ON_PRESS),
-  [5] = ACTION_LAYER_MOMENTARY(1)
+  [4] = ACTION_FUNCTION(1),//ACTION_LAYER_BIT_SET(0, 0b101, ON_PRESS),
+  [5] = ACTION_LAYER_MOMENTARY(1),
+  [6] = ACTION_LAYER_SET(0, ON_PRESS),
+  [7] = ACTION_FUNCTION(2)
 };
 
 // it would be nice for shift-lock to ignore layer two
@@ -25,8 +28,10 @@ const uint16_t PROGMEM fn_actions[] = {
 #define KA_NUMS KC_FN1
 #define KA_SHIFT KC_FN2
 #define KA_FUNS KC_FN3
-#define KA_LOCKNUMS KC_FN4
+#define KA_LOCK1 KC_FN4
 #define KA_MONUMS KC_FN5
+#define KA_CLEAR KC_FN6
+#define KA_LOCK2 KC_FN7
 
 #define KA_BANG SHIFT(KC_1)
 #define KA_DQUO SHIFT(KC_2)
@@ -53,24 +58,39 @@ const uint16_t PROGMEM fn_actions[] = {
   MAP( KC_7,   KC_8, KC_9,    KA_HAT,   KA_PCNT, KA_BANG, KC_LBRC,   KA_AMP,   KC_RBRC, KA_PIPE, \
        KC_4,   KC_5, KC_6,    KA_PND,   KC_MINS, KC_NUHS, KA_LPAR,   KA_DQUO,  KA_RPAR, SHIFT(KC_SCLN), \
        KC_1,   KC_2, KC_3,    KC_EQUAL, KA_PLUS, KC_NUBS, KA_LBRA,   KA_STAR,  KA_RBRA, SHIFT(KC_NUHS), \
-       KC_DOT, KC_0, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KA_MONUMS, KC_GRAVE, KA_DOLR, KC_TRNS)
+       KC_DOT, KC_0, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KA_MONUMS, KC_GRAVE, KA_DOLR, KA_LOCK1)
 
 #define FNS                                                             \
   MAP( KC_HELP,     KC_HOME, KC_UP,   KC_END,   KC_PGUP,   KC_F1,  KC_F2,  KC_F3,   KC_F4,  KC_F5, \
-       KA_LOCKNUMS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,   KC_F6,  KC_F7,  KC_F8,   KC_F9,  KC_F10, \
+       KC_NO,       KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,   KC_F6,  KC_F7,  KC_F8,   KC_F9,  KC_F10, \
        KC_NO,       KC_APP,  KC_NO,   KC_NO,    KC_INSERT, KC_F11, KC_F12, KC_F13,  KC_F14, KC_F15, \
-       KA_BOOT,     KC_NO,   KC_NO,   KC_NO,    KC_DELETE, KC_F16, KC_F17, KC_TRNS, KC_F18, KC_F20)
+       KA_BOOT,     KC_NO,   KC_NO,   KC_NO,    KC_DELETE, KC_F16, KC_F17, KC_TRNS, KC_F18, KA_LOCK2)
+
+#define CLEARS \
+  MAP( KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KA_CLEAR, KA_CLEAR, KC_TRNS, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QWERTY,
   NUMS,
-  FNS
+  FNS,
+  CLEARS
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   switch (id) {
   case 0:
     bootloader();
+    break;
+  case 1:
+    layer_on(1);
+    layer_on(3);
+    break;
+  case 2:
+    layer_on(2);
+    layer_on(3);
     break;
   }
 }
